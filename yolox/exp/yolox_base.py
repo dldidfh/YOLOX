e36cb52c766197874b0db0c81bd496672c7e98f2
yangro <dldidfh2@gmail.com>
2022-06-30 21:54:13 +0900

    modift : val, test root dir path

--- yolox/exp/yolox_base.py
+++ yolox/exp/yolox_base.py
@@ -276,7 +276,7 @@ class Exp(BaseExp):
         valdataset = COCODataset(
             data_dir=self.data_dir,
             json_file=self.val_ann if not testdev else self.test_ann,
-            name="val2017" if not testdev else "test2017",
+            name="" if not testdev else "",
             img_size=self.test_size,
             preproc=ValTransform(legacy=legacy),
         )
